ad70212010fcefb41f487f3a4c70dc6c025a5205
Harshvardhan Singh <130479395+HarshvardhanSingh-13@users.noreply.github.com>
2025-04-03 18:00:08 +0530

    Create main.py

---
+++ Python/1. Introduction to Python/main.py
@@ -0,0 +1,61 @@
+# Online Python compiler (interpreter) to run Python online.
+# print('ABC')
+
+'''
+print(12)
+print(123)
+print("harsh")
+print('12abc')
+'''
+
+
+# Datatypes
+
+'''
+int
+string
+boolean
+float
+'''
+
+# print(False)
+# Arithmetic Operators
+
+## INt - INT OPeration
+# print(12 + 10)
+# print(12 - 10)
+# print(12 * 10)
+# print(12 / 10)
+# print(27 // 10)
+# print(23 % 5)
+# print(5 ** 3)
+
+## INT - STRING
+# print(12 + 'Harsh')
+# print(12 - 'Harsh')
+
+## BOOL - BOOL
+# print(False + True)
+# print(False + True)
+# print(False + True)
+# print(False / True)
+# print(False // True)
+# print(False ** True)
+
+## String - Strung
+
+## Typecasting
+
+## Variables
+
+# first_name = input("Hi Write your first name  ")
+# print(first_name)
+
+## Find Simple Interest 
+P = int(input("Give principle amount"))
+T = int(input('Give Time'))
+R = int(input("Give Rate"))
+
+SI = (P * R * T)/100
+Pnew = P + SI
+print("Your Final Amount is",Pnew)
